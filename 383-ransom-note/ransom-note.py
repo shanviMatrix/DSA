7d59768198fdcf46937f54c6afa0df7600a81fe2
shanviMatrix <0805shanvi@gmail.com>
2026-06-16 21:28:02 +0530

    Time: 3 ms (97.12%) | Memory: 19.6 MB (72.22%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for ch in ransomNote:
+            if ch not in magazine:
+                return False
+
+            magazine = magazine.replace(ch, "", 1)
+
+        return True
