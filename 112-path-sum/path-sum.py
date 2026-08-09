a6eee58c485f4289ae3119d02752ca3e65281264
shanviMatrix <0805shanvi@gmail.com>
2026-08-09 18:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (56.40%) - LeetSync

---
+++ 112-path-sum/path-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasPathSum(self, root, targetSum):
+
+        if root is None:
+            return False
+
+        if root.left is None and root.right is None:
+            return root.val == targetSum
+
+        remaining = targetSum - root.val
+
+        return (
+            self.hasPathSum(root.left, remaining)
+            or
+            self.hasPathSum(root.right, remaining)
+        )
