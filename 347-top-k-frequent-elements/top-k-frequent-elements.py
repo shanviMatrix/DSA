e7210b56213d46d9465ebf977c79a910fd7c5a14
shanviMatrix <0805shanvi@gmail.com>
2026-08-12 22:47:51 +0530

    Time: 7 ms (51.82%) | Memory: 22.9 MB (41.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,19 +3,16 @@ class Solution:
 
         frequency = {}
 
-        # Count frequency
         for number in nums:
             if number in frequency:
                 frequency[number] += 1
             else:
                 frequency[number] = 1
 
-        # Sort numbers according to frequency
         numbers = list(frequency.keys())
 
         numbers.sort(key=lambda x: frequency[x], reverse=True)
 
-        # Take first k numbers
         answer = numbers[:k]
 
         return answer
